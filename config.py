05e24b2b7d3d44b111dec53f61da390ce9c18722
barackdastan-sudo <barackdastan@gmail.com>
2026-08-20 01:45:19 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-from pydantic_settings import BaseSettings
-class Settings(BaseSettings):
-    REDIS_URL: str = "redis://localhost:6379"
-    AES_SECRET_KEY: str = "12345678901234567890123456789012"
-get_settings = Settings()
